0a235e1a16231f801fb4b2d35c62858aba2c7682
priyankaacharyacmaug26-glitch <priyanka.acharya.cmaug26@gmail.com>
2026-09-22 11:46:17 +0530

    Add files via upload

---
+++ java Assignment/ForLoo[Assignment/Program.java
@@ -0,0 +1,14 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		DisplayPattern obj = new DisplayPattern();
+		//obj.pattern1();
+		//obj.pattern2();
+		//obj.pattern3();
+		//obj.pattern4();
+		obj.pattern5();
+
+	}
+
+}
---
+++ java Assignment/ForLoo[Assignment/program.java
@@ -0,0 +1,17 @@
+
+public class program {
+
+	public static void main(String[] args) {
+		DisplayNumber obj=new DisplayNumber();
+		
+		obj.EvenNumber();
+		//obj.OddNumber();
+		//obj.seventhnumber();
+		//obj.pattern1();
+		//obj.pattern2();
+		
+		
+
+	}
+
+}
